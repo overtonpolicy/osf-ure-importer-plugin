e88a892b4590496ddc7fcab908ce2842eba11e73
Kevin Crouse <krcrouse@gmail.com>
2021-10-14 23:29:22 -0400

    adding the rest of the files for the new google auth

---
+++ flaskapp/google.py
@@ -0,0 +1,224 @@
+""" osfapp module for managing Google services 
+
+Google Cloud Dashboard: https://console.cloud.google.com/apis/dashboard?project=uremethods&supportedpurview=project
+Google Credentials are set up here: https://console.developers.google.com/apis/credentials
+
+Callback urls are always /auth/googleauth-callback.html
+
+
+Client Id 138772507523-00fai52l5a5irdg9femjv4gu067o3giq.apps.googleusercontent.com
+client secret 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
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+TODO:
+Create https://uremethods.org/privacypolicy/
+Create https://uremethods.org/termsofuse/
+
+"""
+import yaml, json 
+import requests 
+import flask
+import sys
+import os 
+from . import auth, osf
+
+import oauthlib.oauth2 # just for exceptions
+    
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+
+bp = flask.Blueprint('google', __name__, url_prefix='/google')
+
+
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
+
+
+def get_oauth_details():
+    """ This is the client id that is registered in OSF and tied ot the hostname. """
+    dev_mode = auth.development_level()
+
+    with open('conf/googleauth.yml') as fh:
+        secrets = yaml.load(fh, Loader=yaml.CLoader)
+
+    if dev_mode not in secrets:
+        raise Exception(f"Cannot find mode '{dev_mode}' in the OSF Oauth Configuration")
+    return(secrets[dev_mode])
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def googlelogin():
+    """ This is the Google login webpage, for testing """
+    return(flask.render_template('google/login.html'))
+
+
+def get_scopes(contexts):
+    """ Return a list of Google API scopes based on the context of use. Context is intended to expand as we add more functions
+    Args:
+        contexts (list|str): a contexts or contexts that indicate the scopes
+    """
+    if type(contexts) is not list:
+        contexts = [contexts]
+    
+    # start all scopes with basic profile stuff
+    scopes = [
+        "openid",
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "https://www.googleapis.com/auth/userinfo.email",
+    ]
+
+    for context in contexts:
+        if context == 'import':
+            scopes.extend(["https://www.googleapis.com/auth/drive.readonly", "https://www.googleapis.com/auth/documents.readonly"])
+        else:
+            raise Exception(f"We do not recognize a context of {context}")
+    return(scopes)
+
+@bp.route('/authenticate', methods=('GET', 'POST'))
+def googleauthenticate():
+    """ This is the webpage for the Google authentication window. This is expected to be called in a popup. """
+
+    # Find out what URL to hit for Google login
+    secrets = get_oauth_details()
+    scopes = ' '.join(get_scopes('import'))
+
+    # Use the client_secret.json file to identify the application requesting
+    # authorization. The client ID (from that file) and access scopes are required.
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        'conf/google_client_secret.json',
+        scopes=scopes,
+    )
+
+    # add the redirect url
+    flow.redirect_uri = secrets['url']
+
+    # Generate URL for request to Google's OAuth 2.0 server.
+    authorization_url, state = flow.authorization_url(
+        # Enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type='offline',
+        # Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes='true',
+        # state='' << this is an optional, artbitrary pass through parameter that would get sent to /auth-callback if provided. See https://developers.google.com/identity/protocols/oauth2/web-server#python
+    )
+    return(flask.redirect(authorization_url))
+    
+
+@bp.route("/auth-callback")
+def google_oauth2_callback():
+    """ This is the callback url for Google OAuth2 """
+    
+    secrets = get_oauth_details()
+    #state = flask.session['state']
+    scopes = ' '.join(get_scopes('import'))
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        'conf/google_client_secret.json',
+        scopes=scopes,
+        #state=state,
+    )
+    flow.redirect_uri = secrets['url']
+    authorization_response = flask.request.url
+    try:
+        flow.fetch_token(authorization_response=authorization_response)
+    except oauthlib.oauth2.rfc6749.errors.AccessDeniedError as ae:
+        return({'success':False, 'error': 'Access was not granted. You cannot proceed without with pluging without granting access to your Google account.'})
+    except Warning as warn:
+        return({'success':False, 'error': 'Access was not fully granted. You cannot proceed without with pluging without granting access to your Google account.', 'error_code': str(warn)})
+
+    # Store the credentials in the session.
+    credentials = flow.credentials
+    flask.session['google_token'] = {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'scopes': credentials.scopes,
+    }
+
+    print(f"Google token: {flask.session['google_token']}", file=sys.stderr)
+    return(flask.current_app.send_static_file('auth/googleauth-callback.html'))
+
+@bp.route("/getme", methods=('GET', 'POST'))
+def google_get_authenticated_user():
+
+    if 'google_token' not in flask.session or not flask.session['google_token']:
+        return({'error': "Not logged into Google."})
+    
+    # Load credentials from the session.
+    credentials = google.oauth2.credentials.Credentials(
+        **flask.session['google_token']
+    )
+        
+    user_info_service = googleapiclient.discovery.build(
+        'oauth2','v2', credentials=credentials
+    )
+    user_info = user_info_service.userinfo().get().execute()
+
+    print(f"User info:  {user_info}", file=sys.stderr)
+    
+    return(user_info)
+
+@bp.route("/getfiles", methods=('GET', 'POST'))
+def get_files():
+
+    if 'google_token' not in flask.session or not flask.session['google_token']:
+        return({'error': "Not logged into Google."})
+
+    # Load credentials from the session.
+    credentials = google.oauth2.credentials.Credentials(
+        **flask.session['google_token']
+    )
+
+    name = flask.request.values.get('name_contains')
+    if name:
+        search_value += f' and name contains "{name}"'
+
+    #params={'q': 'mimeType="application/vnd.google-apps.document"'},
+    
+    drive = googleapiclient.discovery.build(
+        'drive', 'v3', credentials=credentials, 
+    )
+
+    files = drive.files().list().execute()
+    return flask.jsonify(**files)
+    
+
+@bp.route('/logout', methods=('GET', 'POST'))
+def googlelogout():
+
+    if 'google_token' not in flask.session or not flask.session['google_token']:
+        raise Exception("You are not presently logged into Google, so you cannot log out.")
+
+    creds = flask.session['google_token']
+    resp = requests.post('https://oauth2.googleapis.com/revoke',
+        params={'token': creds['token']},
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+    )
+    flask.session['google_token'] = None
+    if resp.ok:
+        return({'logout': 'successful'})
+    else:
+        return({
+            'error': ['Failed to log out'],
+            'status_code': resp.status_code,
+            'message': resp.message,
+            'json': resp.json()
+        })
+
+@bp.route('/refresh', methods=('GET', 'POST'))
+def googlerefresh():
+
+
+    authorization_url, state = flow.authorization_url(
+        # Enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type='offline',
+        # Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes='true',
+    )
+
---
+++ flaskapp/static/js/googleauth.js
@@ -0,0 +1,199 @@
+class GoogleAuth {
+
+    /**
+     * 
+     * @param {array} scopes A list of google scopes to request
+     */
+    constructor(scopes){
+        this.loginCallbacks = []
+        this.logoutCallbacks = []
+        this.logged_in = false 
+        this.scopes = scopes;
+    }
+    
+    /** launchAuthWindow - Begin OAUTH2 Authorization
+     * 
+     * @param {function} success - a function to be called if authorization is successful. If all is successful, this function will be called with the authorization token. If the call is valid but the server providdes an error, this function will be called with no parameters.
+     * @param {function} error - a function to be called if the ajax call fails.
+     * 
+     * TODO: Should success_callback really be called in cases of error? 
+     */
+     launchAuthWindow(params){
+        var self = this;
+	    if(!params)
+	        params = {error: ureAjaxError};	  
+	    else if(!params.error)
+            params.error = ureAjaxError;
+
+        //create popup window for authorization
+        var child = window.open(
+            "/google/authenticate",
+            'GoogleOauth',
+            'width=550,height=700'
+        );
+
+        //listener to receive messages from child
+        window.addEventListener(
+            'message',
+            function(event) {
+                console.log("Google Message Received:");
+                console.log(event);
+                if(event.origin !== window.location.origin) 
+                    return;
+
+                if(event.data.authenticated){
+                    child.postMessage('close', window.location.origin); 
+                    self.completeAuthorization({success:params.success, error:params.error});                
+                }
+                else if (event.data.revoked){
+                    child.postMessage('close', window.location.origin); 
+                    if(error_callback)
+                        error_callback();
+                }
+                else{
+                    console.warn("Not authenticated and not revoked");
+                    console.log(event.data);
+                    child.postMessage('close', window.location.origin); 
+                    UREErrorDialog("Received a response from the child, but cannot comprehend it.")                    
+                }
+            },
+            false,
+        );
+    }
+    
+    /** the function called by the authorization window to send the access code back to the parent*/
+    static initializeCallback(){
+        window.opener.postMessage(
+            {'authenticated': true}, 
+            window.location.origin,
+        );
+        window.close();
+    }
+
+
+    /** Completes the authorization by posting to the server to exchange the access code for an authorization token. */
+    completeAuthorization(params){
+        this.refreshLogin(params);
+    }
+
+    logout(params){
+        var self = this;
+        if(!params)
+            params = {error:ureAjaxError};
+        else if(!params.error)
+            params.error = ureAjaxError;            
+
+        $.ajax({
+            url: "/google/logout",
+            method: "POST",
+            success: function(data) {                
+                if(data.error){
+                    params.error(data);                     
+                    if(params.on_error)
+                        params.on_error(data);   
+                }
+                else if (data.errors){                    
+                    params.error(data); 
+                }
+                else{
+                    self.registerLogout(data);
+                    if(params.success)
+                        params.success(data);
+                }
+            },
+            error: params.error
+        });
+    }
+
+    refreshLogin(params){
+        var self = this;
+        if(!params.error)
+            params.error = ureAjaxError;            
+        $.ajax({
+            url: "/google/getme",
+            method: "POST",
+            dataType: "json",
+            data: {},
+            success: function(data) {                
+                if(data.error){
+                    params.error(data);                     
+                    if(params.on_error)
+                        params.on_error(data);   
+                }
+                else if (data.errors){                    
+                    params.error(data); 
+                }
+                else{
+                    if(!self.logged_in){
+                        self.registerLogin(data);
+                        if(params.success)
+                            params.success(data);
+                    }
+                }
+            },
+            error: params.error
+        });     
+    }
+
+    registerLogin(me){
+        var self = this;
+        if(self.logged_in)
+            return;
+        $('#google-login').hide();
+        $('#google-logout').show();
+        $('.google-login-required').each(function(){
+            $(this).show();
+        });
+        $('.hide-after-google-login').each(function(){
+            $(this).hide();
+        });
+        self.logged_in = true;
+        self.me = me;
+        $('#google-authentication-status').html('You are logged in as ' + me.name);
+        self.loginCallbacks.forEach(function(callback){
+            callback(me);
+        });
+    }
+
+    registerLogout(){
+        var self = this;
+        if(!self.logged_in)
+            return;
+        $('#google-login').show();
+        $('#google-logout').hide();
+        $('#google-authentication-status').html('You are not logged in.');
+        $('.google-login-required').each(function(){
+            $(this).hide();
+        });
+        $('.hide-after-google-login').each(function(){
+            $(this).show();
+        });
+
+        self.logged_in = false;  
+        self.me = null;      
+        // update the label and conduct any custom callbacks
+        self.logoutCallbacks.forEach(function(callback){
+            callback();
+        });
+        console.log("Logout complete");
+    }
+}
+
+var google  = new GoogleAuth();
+$(document).ready(function () {
+
+    $('#google-login').click(function(){
+        google.launchAuthWindow();
+    });
+    
+    $('#google-logout').click(function(){
+        google.logout();
+    });
+
+    //
+    // Initialize based on current activity
+    //
+    $('#google-login').show();
+    $('#google-logout').hide();
+    //google.refreshLogin(success=function(me){google.registerLogin(me)} );
+});
