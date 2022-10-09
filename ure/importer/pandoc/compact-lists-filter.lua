6f70bb04161034e06c1e48ed1fbde4a1db2097d7
Kevin Crouse <krcrouse@gmail.com>
2022-10-09 00:03:28 -0400

    adding the filter to remove newlines between list items

---
+++ ure/importer/pandoc/compact-lists-filter.lua
@@ -0,0 +1,34 @@
+-- Source: https://stackoverflow.com/a/57943159/7361270
+-- Modified by makeworld
+
+-- Iterate over all blocks in an item, converting 'top-level'
+-- Para into Plain blocks.
+function compactifyItem (blocks)
+    -- step through the list of blocks step-by-step, keeping track of the
+    -- element's index in the list in variable `i`, and assign the current
+    -- block to `blk`.
+    -- 
+    for i, blk in ipairs(blocks) do
+      if blk.t == 'Para' then
+        -- update in item's block list.
+        blocks[i] = pandoc.Plain(blk.content)
+      elseif blk.t == 'BlockQuote' then
+        -- It's a Google Doc thing, where each bullet is in a blockquote
+        -- https://github.com/jgm/pandoc/issues/6824
+        blocks[i] = pandoc.Plain(blk.content[1].content)
+      end
+    end
+    return blocks
+  end
+  
+  function compactifyList (l)
+    -- l.content is an instance of pandoc.List, so the following is equivalent
+    -- to pandoc.List.map(l.content, compactifyItem)
+    l.content = l.content:map(compactifyItem)
+    return l
+  end
+  
+  return {{
+      BulletList = compactifyList,
+      OrderedList = compactifyList
+  }}
