7547d940a1f76997db213754b7fc7afdc480022e
Kevin Crouse <krcrouse@gmail.com>
2023-02-19 10:11:58 -0500

    updating passenger_wsgi for new osfflask path

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,7 +2,7 @@ import sys, os
 INTERP = os.path.join(os.environ['HOME'], 'pythonenv', 'bin', 'python3')
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
-sys.path.append(os.getcwd())
 
+sys.path.append(os.getcwd() + "/pkg/")
 import osfflask
 application = osfflask.create_app()
