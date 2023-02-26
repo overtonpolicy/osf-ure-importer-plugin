2da1540ba2e0705703d1f4976b30630a9707ffd7
Kevin Crouse <krcrouse@gmail.com>
2023-02-26 04:14:21 -0500

    adding new passenger wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,8 @@
 import sys, os
-INTERP = os.path.join(os.environ['HOME'], 'pythonenv', 'bin', 'python3')
+INTERP = os.path.join(os.getcwd(), 'pythonenv', 'bin', 'python3')
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 
-sys.path.append(os.getcwd() + "/pkg/")
+sys.path.insert(0, os.getcwd() + "/pkg/")
 import osfflask
 application = osfflask.create_app()
