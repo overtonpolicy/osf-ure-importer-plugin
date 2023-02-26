fa908ef64386ce5499c27533fd141eaa60b83146
Kevin Crouse <krcrouse@gmail.com>
2023-02-26 07:18:14 -0800

    updating destination for the last exports

--- pkg/ure-package/ure/exporter.py
+++ pkg/ure-package/ure/exporter.py
@@ -462,9 +462,9 @@ class Docx(BaseExporter):
         preprocessed_md = self.preprocess_markdown(mkdtext)
         ast = self.markdown_analyzer(preprocessed_md)
         stringified = pprint.pformat(ast, width=200, indent=5) 
-        with open('current_mkd.md', 'w') as fh:
+        with open('tmpfiles/current_mkd.md', 'w') as fh:
             fh.write(preprocessed_md)
-        with open('current_ast.js', 'w') as fh:
+        with open('tmpfiles/current_ast.js', 'w') as fh:
             fh.write(stringified)
         
         print(stringified)
