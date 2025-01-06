427c1adc5658ca9e1e6afc64598773ba5b298e9f
Kevin Crouse <krcrouse@gmail.com>
2025-01-05 19:24:25 -0500

    removing old passenger file

--- passenger_wsgi.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-INTERP = os.path.join(os.getcwd(), 'osfvenv', 'bin', 'python3')
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
-
-sys.path.insert(0, os.getcwd() + "/pkg/")
-import osfflask
-application = osfflask.create_app()
