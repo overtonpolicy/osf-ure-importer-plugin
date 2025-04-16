219f68ce7a0d5659d660db15875f8db9883ffc83
stew-overton <stewart@overton.io>
2025-04-16 16:13:10 +0100

    Styling changes and some small js updates

--- pkg/osfflask/static/js/osfauth.js
+++ pkg/osfflask/static/js/osfauth.js
@@ -325,6 +325,7 @@ class OSFAuth {
     registerLogin(me){
         var self = this;
         $('#osf-login').hide();
+        $('#import-options').show();
         $('#osf-logout').show();
         $('.osf-login-required').each(function(){
             $(this).show();
@@ -343,7 +344,7 @@ class OSFAuth {
             // in this case, we logged in separately and are notifying the osf object of it, so we 
             // we don't need to call getme separately.
             self.me = me;
-            $('#osf-authentication-status').html('You are logged in as '+me.attributes.full_name+'.');
+            $('#osf-authentication-status').html('Logged in as '+me.attributes.full_name+'.');
             self.loginCallbacks.forEach(function(callback){
                 callback(me);
             });
@@ -352,7 +353,7 @@ class OSFAuth {
             // update the label and conduct any custom callbacks
             self.getme({success:function(me){            
                 self.me = me;
-                $('#osf-authentication-status').html('You are logged in as '+me.attributes.full_name+'.');
+                $('#osf-authentication-status').html('Logged in as '+me.attributes.full_name+'.');
                 self.loginCallbacks.forEach(function(callback){
                     callback(me);
                 });
@@ -365,8 +366,9 @@ class OSFAuth {
     registerLogout(){
         var self = this;
         $('#osf-login').show();
+        $('#import-options').hide();
         $('#osf-logout').hide();
-        $('#osf-authentication-status').html('You are not logged in.');
+        $('#osf-authentication-status').html('');
         $('.osf-login-required').each(function(){
             $(this).hide();
         });
@@ -417,7 +419,8 @@ $(document).ready(function () {
     //
     $('#osf-login').show();
     $('#osf-logout').hide();
-    osf.getme({ 
+    $('#import-options').hide();
+    osf.getme({
         success: function(me){osf.registerLogin(me)}, 
         error: function(){osf.registerLogout()},
         relogin: false,
